0d85e8b32907dc3b7da640d2984cb6ce67e7e196
a Code Lizard <code.lizard.42@gmail.com>
2014-02-14 12:11:59 +0100

    Whitespace shenanigans

--- adhd-away.user.js
+++ adhd-away.user.js
@@ -1,21 +1,21 @@
 // ==UserScript==
-// @name			ADHD-Away
-// @namespace		github.com/codelizard42
-// @description	Improves both productivity and sleep cycles. Based on the "Productivity Improver" userscript by SuperFancyGentleman at Userscripts.org
-// @version		0.0.1
-// @updateURL	https://github.com/codelizard42/adhd-away/raw/master/adhd-away.user.js
-// @include		http*://reddit.com/*
-// @include		http*://*.reddit.com/*
-// @include		http*://facebook.com/*
-// @include		http*://*.facebook.com/*
-// @include		http*://facepunch.com/*
-// @include		http*://*.facepunch.com/*
-// @include		http*://tumblr.com/*
-// @include		http*://*.tumblr.com/*
-// @include		http*://youtube.com/*
-// @include		http*://*.youtube.com/*
-// @include		http*://vimeo.com/*
-// @include		http*://*.vimeo.com/*
+// @name				ADHD-Away
+// @namespace			github.com/codelizard42
+// @description		Improves both productivity and sleep cycles. Based on the "Productivity Improver" userscript by SuperFancyGentleman at Userscripts.org
+// @version			0.0.1
+// @updateURL		https://github.com/codelizard42/adhd-away/raw/master/adhd-away.user.js
+// @include			http*://reddit.com/*
+// @include			http*://*.reddit.com/*
+// @include			http*://facebook.com/*
+// @include			http*://*.facebook.com/*
+// @include			http*://facepunch.com/*
+// @include			http*://*.facepunch.com/*
+// @include			http*://tumblr.com/*
+// @include			http*://*.tumblr.com/*
+// @include			http*://youtube.com/*
+// @include			http*://*.youtube.com/*
+// @include			http*://vimeo.com/*
+// @include			http*://*.vimeo.com/*
 // ==/UserScript==
 
 var body = document.getElementsByTagName( "body" )[0];
