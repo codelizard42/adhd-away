1efc8c74c33dd5bcb5a97b2fdc3a049d282bfade
a Code Lizard <code.lizard.42@gmail.com>
2014-03-28 13:16:13 +0100

    Only activate at certain times of the day or night

--- adhd-away.user.js
+++ adhd-away.user.js
@@ -2,7 +2,7 @@
 // @name				ADHD-Away
 // @namespace			github.com/codelizard42
 // @description		Improves both productivity and sleep cycles. Based on the "Productivity Improver" userscript by SuperFancyGentleman at Userscripts.org
-// @version			0.0.1
+// @version			0.0.2
 // @updateURL		https://github.com/codelizard42/adhd-away/raw/master/adhd-away.user.js
 // @include			http*://reddit.com/*
 // @include			http*://*.reddit.com/*
@@ -17,10 +17,19 @@
 // @include			http*://vimeo.com/*
 // @include			http*://*.vimeo.com/*
 // ==/UserScript==
+var current		= new Date();
+var hours		= current.getHours();
+
+var morning_hour	= 8;
+var evening_hour	= 23;
 
 var body = document.getElementsByTagName( "body" )[0];
-body.innerHTML = "";
-document.getElementsByTagName( "head" )[0].innerHTML = "";
-body.style.margin = "0";
-body.style.padding = "0";
-window.location.replace("http://heyyeyaaeyaaaeyaeyaa.com/");
+
+if ((hours <= morning_hour) || (hours => evening_hour)) {
+    console.log("HEYEYEYEYAA");
+	body.innerHTML = "";
+	document.getElementsByTagName( "head" )[0].innerHTML = "";
+	body.style.margin = "0";
+	body.style.padding = "0";
+	window.location.replace("http://heyyeyaaeyaaaeyaeyaa.com/");
+}
